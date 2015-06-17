9157b06fd763fc4aac0f2dba4751b63b75b77d3c
ash321win <ash321win@gmail.com>
2015-06-18 00:07:09 +0530

    comments added
    descriptive comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
 ## This file contains two functions that are used to create a special object 
 ## that stores matrix and cache's its inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix, which is actually a list containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse of the matrix
+## 4. get the value of the inverse of the matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
   matrixInv <- NULL
