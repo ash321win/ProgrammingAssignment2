ae9164debec501664bf6684e3a2e8aa77adfa3aa
ash321win <ash321win@gmail.com>
2015-06-17 22:25:01 +0530

    First Try
    cachematrix written similar to cachevector in the example

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  matInv <- NULL
+  
+  setMatrix <- function(y){
+    x <<- y
+    matInv <<- NULL
+  }
+  getMatrix <- function() x
+  
+  setInverseMatrix <- function(inverse) matInv <<- inverse
+  getInverseMatrix <- function() matInv
+  
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInverseMatrix = setInverseMatrix, getInverseMatrix = getInverseMatrix)
 
 }
 
@@ -12,4 +25,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  im <- x$getInverseMatrix() 
+  if(!is.null(im)){
+    message("getting cached inverse matrix")
+    return(im)
+  }
+  
+  data <- x$getMatrix()
+  im <- solve(data, ...)
+  x$setInverseMatrix(im)
+  
+  im
 }
